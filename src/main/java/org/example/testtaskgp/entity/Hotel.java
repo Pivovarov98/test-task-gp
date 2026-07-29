2c388563786c262d99923f4c006fa77cd5f6ceb0
Pivovarov98 <legion_19981@mail.ru>
2026-07-29 22:47:53 +0300

    feat: add hotel entity

---
+++ src/main/java/org/example/testtaskgp/entity/Hotel.java
@@ -0,0 +1,41 @@
+package org.example.testtaskgp.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.example.testtaskgp.entity.enums.Amenities;
+import org.example.testtaskgp.entity.enums.Brand;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Brand brand;
+
+    @Embedded
+    private Address address;
+
+    @Embedded
+    private Contacts contacts;
+
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private List<Amenities> amenities;
+}
