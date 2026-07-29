76c6a2328318f5e5874b2ad05deda4e228bbe05e
Pivovarov98 <legion_19981@mail.ru>
2026-07-29 16:52:45 +0300

    Initial commit

---
+++ src/main/java/org/example/testtaskgp/TestTaskGpApplication.java
@@ -0,0 +1,13 @@
+package org.example.testtaskgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskGpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskGpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/testtaskgp/TestTaskGpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testtaskgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskGpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
