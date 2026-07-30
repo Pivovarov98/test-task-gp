ec65f3d3508335203a701db1527be731632e3616
Pivovarov98 <legion_19981@mail.ru>
2026-07-30 17:51:36 +0300

    feat: add specification executor

--- src/main/java/org/example/testtaskgp/repository/HotelRepository.java
+++ src/main/java/org/example/testtaskgp/repository/HotelRepository.java
@@ -2,6 +2,8 @@ package org.example.testtaskgp.repository;
 
 import org.example.testtaskgp.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
+public interface HotelRepository extends JpaRepository<Hotel, Long>,
+                                         JpaSpecificationExecutor<Hotel> {
 }
