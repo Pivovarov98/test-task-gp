823cf810b511e572157787ee87d52dae95beb4af
Pivovarov98 <legion_19981@mail.ru>
2026-07-31 20:20:26 +0300

    feat: add get method for histogram search

--- src/main/java/org/example/testtaskgp/controller/HotelController.java
+++ src/main/java/org/example/testtaskgp/controller/HotelController.java
@@ -4,6 +4,7 @@ import org.example.testtaskgp.dto.hotel.create_update.AmenitiesDTO;
 import org.example.testtaskgp.dto.hotel.create_update.HotelCreateDTO;
 import org.example.testtaskgp.dto.hotel.response.HotelFullResponseDTO;
 import org.example.testtaskgp.dto.hotel.response.HotelShortResponseDTO;
+import org.example.testtaskgp.dto.hotel.search.HistogramParam;
 import org.example.testtaskgp.dto.hotel.search.HotelSearchFilter;
 import org.example.testtaskgp.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping
@@ -35,13 +37,19 @@ public class HotelController {
         return ResponseEntity.ok().body(hotelService.search(filter));
     }
 
+    @GetMapping("/histogram/{param}")
+    public ResponseEntity<Map<String, Long>> histogram(@PathVariable String param) {
+        return ResponseEntity.ok().body(hotelService.getHistogram(HistogramParam.fromValue(param)));
+    }
+
     @PostMapping("/hotels")
     public ResponseEntity<HotelShortResponseDTO> createHotel(@RequestBody HotelCreateDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.createHotel(dto));
     }
 
     @PostMapping("/hotels/{id}/amenities")
-    public ResponseEntity<HotelFullResponseDTO> addAmenities(@PathVariable Long id, @RequestBody AmenitiesDTO dto){
+    public ResponseEntity<HotelFullResponseDTO> addAmenities(@PathVariable Long id,
+                                                             @RequestBody AmenitiesDTO dto) {
         return ResponseEntity.ok().body(hotelService.addAmenities(id, dto));
     }
 }
