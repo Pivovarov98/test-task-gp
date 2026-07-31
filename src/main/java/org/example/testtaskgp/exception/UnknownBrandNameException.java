e323b7eb9da9df497c7252c5c90ea2889c5b9c98
Pivovarov98 <legion_19981@mail.ru>
2026-07-31 18:53:29 +0300

    feat: unknown brand exception

---
+++ src/main/java/org/example/testtaskgp/exception/UnknownBrandNameException.java
@@ -0,0 +1,7 @@
+package org.example.testtaskgp.exception;
+
+public class UnknownBrandNameException extends IllegalArgumentException {
+    public UnknownBrandNameException(String message) {
+        super(message);
+    }
+}
