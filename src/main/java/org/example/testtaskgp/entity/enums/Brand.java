4489affc449021073020ceabd31cce4cbd095ada
Pivovarov98 <legion_19981@mail.ru>
2026-07-31 18:52:52 +0300

    feat: add method for creat hotel when hotel brand have full brand name in json request

--- src/main/java/org/example/testtaskgp/entity/enums/Brand.java
+++ src/main/java/org/example/testtaskgp/entity/enums/Brand.java
@@ -1,5 +1,11 @@
 package org.example.testtaskgp.entity.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.example.testtaskgp.exception.UnknownBrandNameException;
+
+import java.util.Arrays;
+
 public enum Brand {
     MARRIOTT("Marriott International"),
     JIN_JIANG("Jin Jiang International"),
@@ -7,13 +13,22 @@ public enum Brand {
     INTER_CONTINENTAL("InterContinental Hotel Group"),
     WYNDHAM("Wyndham Hotels & Resorts");
 
-    private final String brand;
+    private final String brandName;
 
-    Brand(String brand) {
-        this.brand = brand;
+    Brand(String brandName) {
+        this.brandName = brandName;
     }
 
-    public String getMessage() {
-        return brand;
+    @JsonValue
+    public String getBrandName() {
+        return brandName;
+    }
+
+    @JsonCreator
+    public static Brand fromBrandName(String brandName) {
+        return Arrays.stream(values())
+                .filter(b -> b.brandName.equalsIgnoreCase(brandName))
+                .findFirst()
+                .orElseThrow(() -> new UnknownBrandNameException("Unknown brand: " + brandName));
     }
 }
