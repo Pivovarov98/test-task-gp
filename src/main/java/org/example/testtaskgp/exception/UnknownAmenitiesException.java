ab80c644e76452b652e7229f307ef72c0a9ae388
Pivovarov98 <legion_19981@mail.ru>
2026-07-31 19:24:53 +0300

    feat: unknown amenities exception

---
+++ src/main/java/org/example/testtaskgp/exception/UnknownAmenitiesException.java
@@ -0,0 +1,7 @@
+package org.example.testtaskgp.exception;
+
+public class UnknownAmenitiesException extends IllegalArgumentException {
+    public UnknownAmenitiesException(String message) {
+        super(message);
+    }
+}
