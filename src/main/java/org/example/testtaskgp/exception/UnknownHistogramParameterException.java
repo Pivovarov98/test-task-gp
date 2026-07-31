7abbefd18871fb60e421732caff42fa19b39c31a
Pivovarov98 <legion_19981@mail.ru>
2026-07-31 20:18:23 +0300

    feat: unknown histogram parameter exception

---
+++ src/main/java/org/example/testtaskgp/exception/UnknownHistogramParameterException.java
@@ -0,0 +1,7 @@
+package org.example.testtaskgp.exception;
+
+public class UnknownHistogramParameterException extends IllegalArgumentException {
+    public UnknownHistogramParameterException(String message) {
+        super(message);
+    }
+}
