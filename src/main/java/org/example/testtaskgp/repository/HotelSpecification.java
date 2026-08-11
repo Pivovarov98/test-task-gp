3573edb7f4da5671f106a0fc8f53793ddeb681c2
Pivovarov98 <legion_19981@mail.ru>
2026-08-11 22:39:05 +0300

    feat: code refactoring

--- src/main/java/org/example/testtaskgp/repository/HotelSpecification.java
+++ src/main/java/org/example/testtaskgp/repository/HotelSpecification.java
@@ -1,8 +1,8 @@
 package org.example.testtaskgp.repository;
 
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
 import org.example.testtaskgp.entity.Hotel;
-import org.example.testtaskgp.entity.enums.Amenities;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
@@ -13,23 +13,24 @@ public class HotelSpecification {
         return (root, query, criteriaBuilder) ->
                 city == null
                         ? null
-                        : criteriaBuilder.equal(criteriaBuilder.lower(root.get("address").get("city")),
-                        city.toLowerCase());
+                        : criteriaBuilder.like(criteriaBuilder.lower(root.get("address").get("city")),
+                        "%" + city.toLowerCase() + "%");
     }
 
     public static Specification<Hotel> hasCountry(String country) {
         return (root, query, criteriaBuilder) ->
                 country == null
                         ? null
-                        : criteriaBuilder.equal(criteriaBuilder.lower(root.get("address").get("country")),
-                        country.toLowerCase());
+                        : criteriaBuilder.like(criteriaBuilder.lower(root.get("address").get("country")),
+                        "%" + country.toLowerCase() + "%");
     }
 
     public static Specification<Hotel> hasBrand(String brand) {
         return (root, query, criteriaBuilder) ->
                 brand == null
                         ? null
-                        : criteriaBuilder.equal(root.get("brand"), brand);
+                        : criteriaBuilder.like(criteriaBuilder.lower(root.get("brand")),
+                        "%" + brand.toLowerCase() + "%");
     }
 
     public static Specification<Hotel> hasName(String name) {
@@ -40,17 +41,22 @@ public class HotelSpecification {
                         "%" + name.toLowerCase() + "%");
     }
 
-    public static Specification<Hotel> hasAmenities(List<Amenities> amenities) {
+    public static Specification<Hotel> hasAmenities(List<String> amenities) {
         return (root, query, criteriaBuilder) -> {
             if (amenities == null || amenities.isEmpty()) {
                 return null;
             }
 
-            Join<Hotel, Amenities> join = root.join("amenities");
+            Join<Hotel, String> join = root.join("amenities");
 
             query.distinct(true);
 
-            return join.in(amenities);
+            List<Predicate> predicates = amenities.stream()
+                    .map(amenity ->
+                            criteriaBuilder.like(criteriaBuilder.lower(join), "%" + amenity.toLowerCase() + "%"))
+                    .toList();
+
+            return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
         };
     }
 }
